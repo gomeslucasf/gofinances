389afec3b9a6890d40d6b5bac88d7dab012f1d2c
Lucas Gomes <gomesfeliplucas@gmail.com>
2022-06-08 02:29:46 -0300

    Firt Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Welcome } from './src/components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome title="HELLO WORLD! ReactNative (Android e IOS)"/>
       <StatusBar style="auto" />
     </View>
   );
