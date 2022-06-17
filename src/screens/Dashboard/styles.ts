38835477e4f59be7760c5ddb64a52c94c5ebd10a
Lucas Gomes <gomesfeliplucas@gmail.com>
2022-06-17 01:52:46 -0300

    finalizando a utilização do styled-components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components/native";
+import {FlatList}  from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage,RFValue } from "react-native-responsive-fontsize";
-import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {getBottomSpace, getStatusBarHeight} from 'react-native-iphone-x-helper';
+
+import { DataListProps} from '.';
 
 export const Container = styled.View`
     flex: 1;
@@ -66,3 +69,25 @@ export const HighlightCards = styled.ScrollView.attrs({
     position:absolute;
     margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions =  styled.View`
+    flex:1;
+    padding: 0 24px;
+    margin-top: ${RFPercentage(8)}px;
+
+`;
+
+export const Title = styled.Text`
+    font-size:${RFValue(18)}px;
+    font-family: ${({theme})=> theme.fonts.regular};
+    margin-bottom:16px;
+`;
+
+export const TransactionList = styled(
+    FlatList as new ()=> FlatList<DataListProps>
+    ).attrs({
+    showsHorizontalScrollIndicator:false,
+    contentContainerStyle:{
+        paddingBottom: getBottomSpace()
+    }
+})``;
